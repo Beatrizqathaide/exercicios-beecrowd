b55fb367cea483145e80c804b3f2047215132463
BEATRIZ <beatrizzquaresma@gmail.com>
2023-06-18 18:46:46 -0400

    responde as perguntas

---
+++ lista5/118.i-am-toorg/resposta.py
@@ -0,0 +1,15 @@
+'''
+Toorg é o membro mais sábio do grupo de heróis chamado Os Protetores da Via Láctea. Para qualquer questão, tem a resposta ideal! Escreva um programa que, dada uma pergunta, diga a resposta de Toorg.
+
+Entrada
+A entrada terá vários casos de teste. Para cada caso de teste, é exibido um número N, que representa o número de casos de teste. Então haverá N linhas, com perguntas feitas para Toorg.
+
+Saída
+Para cada caso de teste, imprima a resposta de Toorg.
+'''
+
+n = int(input())
+
+for c in range(0, n):
+    p = input()
+    print('I am Toorg!')
