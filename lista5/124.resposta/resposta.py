4c8f69588a6100842fe0d0379d4195194805bfb3
BEATRIZ <beatrizzquaresma@gmail.com>
2023-06-19 15:53:51 -0400

    resposta para o professor

---
+++ lista5/124.resposta/resposta.py
@@ -0,0 +1,16 @@
+'''
+Cacunda, Bizz e Massacote são amigos inseparáveis. Na faculdade, em poucos dias, eles não iam à aula para pregar peças. Um dia um professor estava passando. Ao mesmo tempo, os três gritaram bem alto a palavra "gzuz". Depois desse choro, eles ficaram invisíveis e a professora não os viu. Mais uma vez, a turma estava respondendo às perguntas do professor. Quando era a vez de um deles, eles respondiam com a palavra “gzuz”, e o professor aceitava a resposta e dava a nota máxima da questão. Faça a simulação da saída que encontraram para sair dos mais diversos problemas.
+
+Entrada
+A entrada é composta por vários casos de teste. A primeira linha contém um inteiro C (2 <= C <= 99) relativo ao número de perguntas que o professor fez. As seguintes linhas C vêm com uma pergunta feita pelo professor.
+
+Saída
+Para cada pergunta, imprima a resposta que foi dita pelos três amigos.
+'''
+
+t = int(input())
+
+for c in range(0, t):
+    p = input()
+    
+    print('gzuz')
