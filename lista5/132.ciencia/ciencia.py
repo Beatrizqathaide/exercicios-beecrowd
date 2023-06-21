83c527e1f08cba1724d79f083362b34f2c5033b8
BEATRIZ <beatrizzquaresma@gmail.com>
2023-06-21 12:07:05 -0400

    mostra mensagem

---
+++ lista5/132.ciencia/ciencia.py
@@ -0,0 +1,21 @@
+'''
+Oronob é um menino muito peculiar. Para ele, apenas sua opinião importa e nada mais. Durante a pandemia do Coronga Vírus, ele usa sua opinião para retaliar o que a ciência diz. Ele então, para aumentar suas opiniões, criou um bot (robô que gera mensagens automáticas) para acionar seus textos para muitas pessoas.
+
+Você conhece Oronob, mas ao contrário dele, você é sensato e sabe que a ciência não se refuta com opinião, assim como entendendo de programação, então decidiu hackear o bot do Oronob para evitar suas falácias.
+
+Bem, então crie um algoritmo que mude o texto das opiniões de Oronob para a verdade real.
+
+Entrada
+Um texto S com caracteres alfanuméricos e possíveis espaços entre as palavras.
+
+Limites: 0 <| S | <= 280.;)
+
+Saída
+A verdade.
+'''
+
+s = input()
+
+s = 'Nao se refuta ciencia com opiniao'
+
+print(s)
