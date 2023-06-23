6c8f238100df46c14e41e56331335a1f0054331a
BEATRIZ <beatrizzquaresma@gmail.com>
2023-06-23 17:25:02 -0400

    mostra qual curso deve fazer

---
+++ lista5/142.curso/curso.py
@@ -0,0 +1,18 @@
+'''
+Não há ninguém no mundo mais indeciso que Vitória. Apesar de saber que é uma grande programadora, como uma das que tem muitos projetos na área de TI publicados e muitos outros sendo escritos, ela não tem certeza se vai ser uma programadora profissional. Tem noites que ela quer estudar Ciência da Computação, tem dias que ela quer fazer um pouco de Engenharia, tem dias que ela até fala em fazer algum curso de Humanidades, que pecado! Mas você pode ajudá-la a fazer a escolha certa. Sua tarefa é simples, será dada uma lista com os cursos de bacharelado que Vitória está em dúvida, a saída deverá ser o curso que ela deve escolher.
+
+Entrada
+A primeira linha da entrada é um inteiro 1 ≤ n ≤ 2000, representando o número de cursos que devem ser considerados. Cada uma das próximas n linhas, há um curso S , 1 ≤ | S | ≤ 100, o nome de um dos cursos Vitória está em dúvida.
+
+Saída
+Imprima o nome do curso que Vitória deve fazer em português sem acentuação.
+
+
+'''
+
+c = int(input())
+
+for i in range(0, c):
+    n = input()
+
+print('Ciencia da Computacao')
