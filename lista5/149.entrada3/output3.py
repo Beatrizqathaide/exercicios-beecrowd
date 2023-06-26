5f5a001d9b6555f0a820ab4ea4174358a017c3a4
BEATRIZ <beatrizzquaresma@gmail.com>
2023-06-25 21:59:57 -0400

    retangulo com equacao no inicio, meio e fim

--- lista5/149.entrada3/output3.py
+++ lista5/149.entrada3/output3.py
@@ -2,31 +2,33 @@
 Seu professor gostaria de fazer uma tela com as seguintes características:
 
 Ter 39 traços (-) na primeira linha;
-Tenha um | abaixo do primeiro travessão e do trigésimo nono travessão da primeira linha, abaixo do travessão deve começar a escrever a palavra "Roberto" e o restante preencher no meio com espaço em branco;
+Tenha um | abaixo do primeiro traço e do trigésimo nono traço da primeira linha, abaixo do 2º traço você deve começar digitando "x = 35" e o restante preencher com espaços em branco;
 Tenha um | abaixo do primeiro traço e do trigésimo nono traço da primeira linha, preencha o meio com um espaço em branco;
-Tenha um | abaixo do primeiro travessão e do trigésimo nono travessão da primeira linha, abaixo do 10 travessão você deve começar a digitar o número "5786" e o restante preencher no meio com espaço em branco;
+Tenha um | abaixo do primeiro traço e do trigésimo nono traço da primeira linha, abaixo do 17º traço você deve começar digitando "x = 35" e o restante preencher com espaços em branco;
 Repita o procedimento 3;
-Tenha um | abaixo do primeiro travessão e do trigésimo nono travessão da primeira linha, abaixo do travessão 10 deve-se começar a escrever a palavra "UNIFEI" e o restante preencher no meio com espaço em branco;
+Tenha um | sob o primeiro traço e o trigésimo nono traço da primeira linha, sob o 33º traço você deve começar digitando "x = 35" e o restante preencher o meio com espaços em branco;
 Repita o procedimento 1.
 Ao final deve ficar como na imagem a seguir:
 
---------------------------------------- (39 traces)
-|        Roberto                      |
+--------------------------------------- (39 dashes)
+
+|x = 35                               |
+
 |                                     |
-|        5786                         |
+
+|                x = 35               |
+
 |                                     |
-|        UNIFEI                       |
---------------------------------------- (39 traces)
+
+|                               x = 35|
+
+--------------------------------------- (39 dashes)
 Entrada
 Não há.
 
 Saída
 A saída será impressa como mostrado acima.
 '''
-
-
-
-
 x = 'x = 35'
 t = '|'
 
@@ -52,3 +54,4 @@ for cont in range(0, 7):
 
     elif cont == 2 or cont == 4:
         print(f'{t:<1}{t:>38}')
+
