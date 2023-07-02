f2a049175d830842af765dbe8fdb17b421a9eb8d
BEATRIZ <beatrizzquaresma@gmail.com>
2023-07-01 22:20:36 -0400

    minha funcao replace

--- lista5/152.replace/replace.py
+++ lista5/152.replace/replace.py
@@ -1,19 +1,23 @@
 a = 'Dona Ana é bem velhinha'
 
-lista = []
+def meuReplace(string, troca, substituto):
+    lista = []
 
-#transforma a string em lista
-for i in range(0, len(a)):
-    lista.append(a[i])
+    #transforma a string em lista
+    for i in range(0, len(string)):
+        lista.append(string[i])
 
-#localiza o elemento a ser trocado e faz a troca
-for c in range(0, len(lista)):
-    if lista[c] == 'a':
-        lista[c] = '*'
+    #localiza o elemento a ser trocado e faz a troca
+    for c in range(0, len(lista)):
+        if lista[c] == troca:
+            lista[c] = substituto
 
-#faz o print com a troca do elemento
-for l in range(0, len(lista)):
-    if l < len(lista) - 1:
-        print(lista[l], end='')
-    if l == len(lista) - 1:
-        print(lista[l])
+    
+    #faz o print com a troca do elemento
+    for l in range(0, len(lista)):
+        if l < len(lista) - 1:
+            print(lista[l], end='')
+        if l == len(lista) - 1:
+            print(lista[l])
+
+print(meuReplace(a, 'a', '*'))
